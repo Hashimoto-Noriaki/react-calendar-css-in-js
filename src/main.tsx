e3022e3e5484fabe09faaa3013babf694b6f094e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-03 01:20:08 +0900

    styled-componentsが導入できているか確認

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import styled from "styled-components";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Container = styled.div`
+  display: flex;
+  justify-content: center;  /* 横方向中央 */
+  align-items: center;      /* 縦方向中央 */
+  height: 100vh;            /* 画面全体を使う */
+`;
+
+const Title = styled.h1`
+  font-size: 1.875rem; /* text-3xl */
+  line-height: 2.25rem;
+  font-weight: 700; /* bold */
+  text-decoration: underline;
+  color: skyblue; /* テキストスカイブルー */
+`;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Container>
+      <Title>Hello world!</Title>
+    </Container>
+  </React.StrictMode>
+);
