4d23ff255a93f3b56967b1d12cdcff56f6f4d174
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-03 01:34:14 +0900

    トップ画面に表示

---
+++ src/components/pages/TopPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const TopPage = () => {
+    return (
+        <div>
+            <header>
+                <div>
+                    <p>スケジュール管理App</p>
+                    <nav>
+                        <ul>
+                            <li>ご利用方法</li>
+                            <li>ログイン</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;  /* 横方向中央 */
-  align-items: center;      /* 縦方向中央 */
-  height: 100vh;            /* 画面全体を使う */
-`;
-
-const Title = styled.h1`
-  font-size: 1.875rem; /* text-3xl */
-  line-height: 2.25rem;
-  font-weight: 700; /* bold */
-  text-decoration: underline;
-  color: skyblue; /* テキストスカイブルー */
-`;
+import { TopPage } from './components/pages/TopPage'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Container>
-      <Title>Hello world!</Title>
-    </Container>
+    <TopPage/>
   </React.StrictMode>
 );
